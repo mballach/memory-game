259b8669c566c99170b3d0655c11ffe850655e83
mballach <muggins111@live.ca>
2025-05-13 16:53:32 -0700

    Added card shuffling, and toggling card status on clicked

--- src/components/MemoryCards.jsx
+++ src/components/MemoryCards.jsx
@@ -1,49 +1,142 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/MemoryCards.css'
 
 
 
 function MemoryCards() {
     const [dataReady, setDataReady] = useState(false);
-    let teams = []
+    const [error, setError] = useState(false)
+    const [teams, setTeams] = useState(null)
+    const [score, setScore] = useState(null)
+    
+    useEffect(()=> {
     async function returnTeamData(){
         try {const response = await fetch('https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams')
             let data = await response.json()
-            let ret = data.sports[0].leagues[0].teams
-            for (let x=0; x<ret.length ; x++){
-                teams.push(ret[x])
-                }
+            data = data.sports[0].leagues[0].teams
+            let teams = []
+            for (let i=0; i<data.length; i++){
+                let j = data[i].team
+                j.clicked = false
+                teams.push(j)
+            }
+            setTeams(teams)
             console.log(teams)
             setDataReady(true)
         } catch (error) {
+            setError(true)
             console.error("Error fetching data:", error)
         }
     }  
-    
     returnTeamData()
-
+    },[]);
     
-    function Card(){
-        return (
+    const shuffleTeams = () => {
+        let newAr = [...teams]
+        let currentIndex = teams.length;
+        while (currentIndex != 0) {
+            // Pick a remaining element...
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [teams[currentIndex], teams[randomIndex]] = [
+            teams[randomIndex], teams[currentIndex]];
+        }
+        setTeams([...teams])
+    }
+
+    const incScore = () => {
+        setScore(score+1)
+        console.log("Current is "+(score+1))
+    }
+
+    const wipeScore = () => {
+        setScore(0);
+    }
+
+    const setClicked = (id) => {
+        const target = teams.filter((team)=>team.id == id)
+        target[0].clicked = true
+        const reduced = teams.filter((team)=>team.id !== id)
+        setTeams([...reduced, ...target])
+    }
+
+    const resetClicked = () => {
+        let newTeams = [...teams]
+        for (let i=0;i<newTeams.length;i++){
+            newTeams[i].clicked=false
+        }
+        console.log(newTeams)
+        setTeams(newTeams)
+        shuffleTeams()
+    }
+
+    function Card({team}){
+
+        const toggleFirstClick = () => {
+            setClicked(team.id)
+            shuffleTeams()
+            incScore()
+            console.log(score+1)
+        }
+
+        const toggleSecondClick = () => {
+            console.log("You already clicked here")
+            incScore()
+        }
+
+        if(team.clicked===false){
+            return (
+                <div className="card" key={team.id} onClick={toggleFirstClick}>
+                            <img className='logo' src={team.logos[0].href} alt="" />
+                            {team.displayName}
+                            Unclicked
+                            
+                </div>
+        )} else {
+            return (
+                <div className="card" key={team.id} onClick={toggleSecondClick}>
+                            <img className='logo' src={team.logos[0].href} alt="" />
+                            {team.displayName}
+                            Clicked
+                            
+                </div>
+            )
+        }
+    }
+
+    if(error){
+        return(
             <>
-                {teams[0]}
+             Error loading cards...
             </>
         )
     }
 
-    if(dataReady){
-
-    return (
-        <>
-           <Card />
-        </>
-    )
-    } else {
+    if(!dataReady) {
         return (
             <>
-            Loading
+            Loading...
             </>
         )
+        
+    }
+
+    if(dataReady){
+        return (
+            <>
+            <button onClick={resetClicked}>Click to reset</button>
+                <div className="card-container">
+                    {teams.map((item) => (
+                        <Card team={item} key={item.id}/>
+                        
+                    ))}
+                    
+                </div>
+            </>
+        )
+
     }
 }
 
